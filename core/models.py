2b8d23a1b265d9661dac5fb869a4c86e17719d3e
reema <reema.angural786@gmail.com>
2019-03-05 10:07:54 -0800

    models changes

--- core/models.py
+++ core/models.py
@@ -12,9 +12,12 @@ board = (
     ('s', 'PSEB')
 )
 
+
+
 class School(Model):
     name = models.CharField(max_length=15)
     board = models.CharField(choices=board, max_length=10)
+
 
     def __str__(self):
         return self.name
